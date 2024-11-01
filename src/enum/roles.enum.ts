b9d5c71b87ed1c1276efce837d2d6dfdae6bc15a
JBernal27 <jomabeye@gmail.com>
2024-10-31 20:17:56 -0500

    add permissions and weather api

--- src/enum/roles.enum.ts
+++
@@ -1,4 +0,0 @@
-export enum Roles{
-    EMPLEADO = 'Empleado',
-    CLIENTE = 'Cliente',
-}
--- src/utilities/format-number.utility.ts
+++
@@ -1,3 +0,0 @@
-export function formatPhoneNumber(number: number): string {
-    return number.toString().replace(/(\d{3})(\d{3})(\d{4})/, '$1 $2 $3');
-}
